0e4c65bdc35d09b36f1a4c8fef9fa75d2947044c
Rukasu25 <GeniuxProx@gmail.com>
2025-10-06 16:59:08 -0300

    inicio
    eso es, el inicio

---
+++ Tarea2.cpp
@@ -0,0 +1,17 @@
+using namespace std;
+#include <string>
+#include <iostream>
+#include <fstream>
+
+Struct Carta {
+char Palo ;
+int Categoria ;
+int Valor ;
+bool Jugada ;
+};
+
+
+int main(){
+    cout<< "Hola Mundo" << endl;
+    return 0;
+}
