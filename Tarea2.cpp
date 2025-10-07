9460e33e9ab2722a91745bce4072bd4b0738033b
Rukasu25 <GeniuxProx@gmail.com>
2025-10-06 23:58:19 -0300

    avance 1
    traté de hacer los structs y dejar avanzada una funcion para barajar

--- Tarea2.cpp
+++ Tarea2.cpp
@@ -2,16 +2,64 @@ using namespace std;
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <random>
 
-Struct Carta {
-char Palo ;
-int Categoria ;
-int Valor ;
-bool Jugada ;
+struct carta {
+    char Palo ;
+    int Categoria ;
+    int Valor ;
+    bool Jugada ;
+};
+
+struct arbolpinta {
+    carta carta;
+    arbolpinta *izq;
+    arbolpinta *der;
+};
+
+struct nodomazo {
+    carta mano;
+    nodomazo *sig;
 };
 
 
+
+
+
+
 int main(){
-    cout<< "Hola Mundo" << endl;
+    carta micarta;
+    micarta.Palo;
+    micarta.Categoria;
+    micarta.Valor;
+    micarta.Jugada;
+
+    arbolpinta* arboles[4] = {nullptr, nullptr, nullptr, nullptr};
+    nodomazo* mazo = nullptr;
+    nodomazo* mano = nullptr;
+    barajar(mazo, arboles);
+
+
+
+
     return 0;
+}
+
+/*funcion para barajar el mazo de cartas en la lista enlazada, debe retornar el mazo barajado*/
+void barajar(nodomazo* &mazo, arbolpinta* arboles[]){
+    carta cartas[52];
+    int indice = 0;
+    //revisar
+    /*
+    for(int palo = 0; palo < 4; palo++){
+        for(int categoria = 1; categoria <= 13; categoria++){
+            cartas[indice].Palo = (palo == 0) ? 'C' : (palo == 1) ? 'D' : (palo == 2) ? 'E' : 'T';
+            cartas[indice].Categoria = categoria;
+            cartas[indice].Valor = (categoria > 10) ? 10 : categoria;
+            cartas[indice].Jugada = false;
+            indice++;
+        }
+
+    }*/
+
 }
